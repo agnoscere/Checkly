028a2f763b88e3e90651b3559b031d6626d26ce9
Elijah Johnson <elijahj1313@gmail.com>
2020-02-22 18:29:41 -0500

    SHUT UP ERIC

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', function() {
+    setTimeout(() => {
+        var h2 = document.getElementById("help");
+        h2.innerHTML = "HELLOOOOOOOOOOOO";
+    }, 2000);
+});
