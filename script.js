42dcb7ffc0c99694dbf6b48fadc28a490d1d64c0
Elijah Johnson <elijahj1313@gmail.com>
2020-02-22 20:07:50 -0500

    Transitions to main popout

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 window.addEventListener('DOMContentLoaded', function() {
-    setTimeout(() => {
-        var h2 = document.getElementById("help");
-        h2.innerHTML = "HELLOOOOOOOOOOOO";
-    }, 2000);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("demo").innerHTML = this.responseText;
+      }
+    };
+
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var link = "http://10.22.0.183/hack2020/process.php?url=" + tabs[0].url;
+
+        xhttp.open("GET", link, true);
+        xhttp.send();
+    });
+
+    var myVar = setTimeout(showPage, 2000);
+          
+    function showPage() {
+        document.getElementById("loadingPage").style.display = "none";
+        document.getElementById("resultPage").style.display = "block";
+    }
 });
